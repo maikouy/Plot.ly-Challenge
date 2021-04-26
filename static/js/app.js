ae88d113c3e477e7f61664049530eedbe91a370a
Mai Kou <maikouyang@gmail.com>
2021-04-26 15:50:29 -0500

    Added an event handler and stubs for the functions.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,32 @@
 console.log("apps.js loaded!");
 
+// This is call a function stub --> Placeholder for the real 'Function'
+function DrawBargraph(sampleId) {
+    console.log(`DrawBargraph(${sampleId})`);
 
+}
+
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechart(${sampleId})`);
+} 
+
+function ShowMetadata(sampleId) {
+    console.log(`ShowMetadata(${sampleId})`);
+}
+
+// When choosing a diff subject on drop-down, it doesnt work as we need 
+// an event handler.
+// Per index.html: onchange="optionChanged(this.value)" this is the event handler
+
+function optionChanged(newSampleId) {
+    console.log(`User selected ${newSampleId}`);
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+    
+}
 
 // Codes below are Dom's starter code shown in office hour:
 function InitDashboard() {
@@ -19,6 +45,12 @@ function InitDashboard() {
             .text(sampleId)
             .property("value", sampleId);
         });
+
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+        DrawBubblechart(id);
+        ShowMetadata(id);
 
     });
 
