a53615fa0593e4fefca39b107e9e5330407b2f22
Mai Kou <maikouyang@gmail.com>
2021-04-27 22:51:07 -0500

    Created code to showcase bubble chart location on dashboard.

--- static/js/app.js
+++ static/js/app.js
@@ -10,21 +10,22 @@ function DrawBargraph(sampleId) {
         // You can omit the console.log line up there bc thats just to check 
         // your work
 
+
         // Now is time to get value out of your data
         // We are ask to graph the 'samples' portion of the data(info)
         var samples = data.samples;
         var resultArray = samples.filter(s => s.id == sampleId);
-        console.log(resultArray);
+        // console.log(resultArray);
         var result = resultArray[0];
-        console.log(result);
+        // console.log(result);
 
         var otu_ids = result.otu_ids;
-        console.log(otu_ids);
+        // console.log(otu_ids);
         var otu_labels = result.otu_labels;
-        console.log(otu_labels);
+        // console.log(otu_labels);
         var sample_values = result.sample_values;
-        console.log(sample_values);
-
+        // console.log(sample_values);
+        
         yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
 
         // Graphs
@@ -44,13 +45,45 @@ function DrawBargraph(sampleId) {
         }
 
         Plotly.newPlot("bar", barArray, barLayout);
-    })  
+
+    })
 }
 
-
+// *****WORK ON THIS PART NOW******
 function DrawBubblechart(sampleId) {
     console.log(`DrawBubblechart(${sampleId})`);
-} 
+
+    d3.json("samples.json").then(data => {
+        console.log(data);
+
+        var trace1 = {
+            x: ['otu_ids'],
+            y: ['samples'],
+            text: ['otu_labels'],
+            mode: 'markers',
+            marker: {
+                size: ['samples'],
+                color: ['otu_ids']
+            }
+        }
+
+        var chartArray = [trace1];
+
+        var chartLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            showlegend: false,
+            heighht: 600,
+            width: 600
+        };
+
+        Plotly.newPlot("bubble", chartArray, chartLayout);
+
+    })
+};
+
+    
+    
+
 
 function ShowMetadata(sampleId) {
     console.log(`ShowMetadata(${sampleId})`);
